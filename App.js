9c7a388db1d290f900ebfc2a9b6a4bc284730731
dulajkavinda <dulajkaavinda@gmail.com>
2021-10-14 21:22:42 +0530

    ui done

--- App.js
+++ App.js
@@ -1,21 +1,132 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Image,
+  View,
+  Platform,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const deleteImage = () => {
+    setImage(null);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+
+        width: "100%",
+        height: "100%",
+
+        backgroundColor: "#353432",
+      }}
+    >
+      {image ? (
+        <Image
+          source={{ uri: image }}
+          style={{ width: 300, height: 300, backgroundColor: "black" }}
+        />
+      ) : (
+        <Image style={{ width: 300, height: 300, backgroundColor: "black" }} />
+      )}
+
+      <TouchableOpacity
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+
+          width: 300,
+          height: 40,
+          backgroundColor: "#9A159A",
+          borderRadius: 10,
+
+          marginTop: 10,
+        }}
+        onPress={pickImage}
+      >
+        <View>
+          <Text style={{ fontSize: 15, fontWeight: "700", color: "white" }}>
+            UPLOAD
+          </Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+
+          width: 300,
+          height: 40,
+          backgroundColor: "#9A159A",
+          borderRadius: 10,
+
+          marginTop: 10,
+        }}
+        onPress={deleteImage}
+      >
+        <View>
+          <Text style={{ fontSize: 15, fontWeight: "700", color: "white" }}>
+            DELETE
+          </Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+
+          width: 300,
+          height: 40,
+          backgroundColor: "#9A159A",
+          borderRadius: 10,
+
+          marginTop: 10,
+        }}
+        onPress={pickImage}
+      >
+        <View>
+          <Text style={{ fontSize: 15, fontWeight: "700", color: "white" }}>
+            PREDICT
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
